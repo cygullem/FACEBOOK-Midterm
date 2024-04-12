216c1a9879b7172a9f9c8ca93f8c20bb44a132f0
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-13 00:22:39 +0800

    mainpage layout

--- mainpage.php
+++ mainpage.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['email'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="png" href="./Assets/Facebook-Logo.png">
     <title>Welcome to Chatnote</title>
     <link rel="stylesheet" href="./CSS/mainpage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -24,7 +25,9 @@ if (!isset($_SESSION['email'])) {
 
 <body>
     <nav>
-
+        <div class="fb-search"></div>
+        <div class="fb-content"></div>
+        <div class="fb-menu"></div>
     </nav>
 
     <section>
