a148bfbf796eb62626642b49d5fb6e5b7fa0c03e
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-13 09:41:18 +0800

    search bar

--- mainpage.php
+++ mainpage.php
@@ -20,12 +20,20 @@ if (!isset($_SESSION['email'])) {
     <title>Welcome to Chatnote</title>
     <link rel="stylesheet" href="./CSS/mainpage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <nav>
-        <div class="fb-search"></div>
+        <div class="fb-search">
+            <div class="logo">
+                <img src="./Assets/Facebook-Logo.png" alt="">
+            </div>
+            <div class="search-bar">
+                <form action="">
+                    <input type="text">
+                </form>
+            </div>
+        </div>
         <div class="fb-content"></div>
         <div class="fb-menu"></div>
     </nav>
