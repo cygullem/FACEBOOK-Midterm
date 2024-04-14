ac3cef2946ad2e8b86db972d884cfdf6913b640c
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-14 15:42:09 +0800

    Posting div

--- JS/script.js
+++ JS/script.js
@@ -100,3 +100,16 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+// POSTING SCRIPT
+const container = document.querySelector(".container"),
+privacy = container.querySelector(".post .privacy"),
+arrowBack = container.querySelector(".audience .arrow-back");
+privacy.addEventListener("click", () => {
+container.classList.add("active");
+});
+arrowBack.addEventListener("click", () => {
+container.classList.remove("active");
+});
