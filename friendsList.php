df3874c80499374b432311a276fe43dcd2c35bac
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-21 12:01:09 +0800

    followers container

--- friendsList.php
+++ friendsList.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body>
+<body id="link">
     <nav>
         <div class="fb-search">
             <div class="logo">
@@ -26,7 +26,7 @@
             </div>
         </div>
         <div class="fb-content">
-            <a href="mainpage.php">
+            <a href="mainpage.php" id="myLink">
                 <div class="FBC fbchome">
                     <i class='bx bx-home-alt' id="homeIcon"></i>
                 </div>
@@ -112,7 +112,29 @@
                 </div>
             </div>
             <div class="flcr_container">
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
 
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
+                <div class="user_follow"></div>
             </div>
         </div>
     </section>
