78443b4349f2b83e55fa5f7003b6f467dea8a4a8
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-22 00:09:00 +0800

    updated the database

--- DATABASE/gullemfacebook.sql
+++ DATABASE/gullemfacebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2024 at 05:46 PM
+-- Generation Time: Apr 21, 2024 at 06:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,6 +45,7 @@ CREATE TABLE `login_table` (
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `salt` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -53,10 +54,11 @@ CREATE TABLE `login_table` (
 -- Dumping data for table `login_table`
 --
 
-INSERT INTO `login_table` (`id`, `firstname`, `lastname`, `email`, `password`, `salt`) VALUES
-(4, 'Cy ', 'Gullem', 'cygullem@gmail.com', '$2y$10$.R/U9OeZph1cigYmzAXhW.HRInKe.jnllduTjg6Jl4wMlD0Pogwx6', '47b31cc6d1bcb532176de37209f12b9c'),
-(5, 'cy ', 'gullem', 'cy@gmail.com', '$2y$10$gsxXdR3OvR8ZERo3Eusvyeys6.DlXZ4Gsxzz6uvTYgocbrtOZ59o2', 'b4018c35bf69e7eb33078123b1e70878'),
-(6, 'Junalisa ', 'Jumao-as', 'junalisa@gmail.com', '$2y$10$t6HQHFyVkIlU7vhvuBO3nuob.fY018zRUfy/sCMIGOoEetlH2KVj6', '0aa3706baa30b10097472dbbbe6f8978');
+INSERT INTO `login_table` (`id`, `firstname`, `lastname`, `email`, `profile_picture`, `password`, `salt`) VALUES
+(4, 'Cy ', 'Gullem', 'cygullem@gmail.com', './Assets/default-profilepicture.png', '$2y$10$.R/U9OeZph1cigYmzAXhW.HRInKe.jnllduTjg6Jl4wMlD0Pogwx6', '47b31cc6d1bcb532176de37209f12b9c'),
+(6, 'Junalisa ', 'Jumao-as', 'junalisa@gmail.com', './Assets/default-profilepicture.png', '$2y$10$t6HQHFyVkIlU7vhvuBO3nuob.fY018zRUfy/sCMIGOoEetlH2KVj6', '0aa3706baa30b10097472dbbbe6f8978'),
+(7, 'Dhaniel', 'Malinao', 'dhaniel@gmail.com', './Assets/default-profilepicture.png', '$2y$10$mFTFSUv7AYHfbVfbWel4SeohLvWSsqivPmH07Mr/CscleP1ERP0Pq', 'e431a4b54bb1d18d6b4637763fc44531'),
+(8, 'Nico', 'Edisan', 'nico@gmail.com', './Assets/default-profilepicture.png', '$2y$10$6IrfoBAaXVrw9TUD0XSeWumsJ5DoBVRaG3Y4LtnWhhigX6SEctrdi', '76d46ea9b2e50db47f1291f4cb067726');
 
 --
 -- Indexes for dumped tables
@@ -88,7 +90,7 @@ ALTER TABLE `follow_table`
 -- AUTO_INCREMENT for table `login_table`
 --
 ALTER TABLE `login_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
