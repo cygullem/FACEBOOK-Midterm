7cc13f7be0fd4d4981f8e29ef358d83a449b1b9c
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-05-04 23:11:16 +0800

    mainpage design testing

--- mainpage.php
+++ mainpage.php
@@ -228,6 +228,7 @@ if ($user) {
                     </div>
                 </div>
             </div>
+
             <div class="users_Posts">
                 <div class="usrsP_1">
                     <div class="usrsp1left">
@@ -283,6 +284,63 @@ if ($user) {
                     </div>
                 </div>
             </div>
+
+            <div class="users_Posts">
+                <div class="usrsP_1">
+                    <div class="usrsp1left">
+                        <div class="usrsp1left_01">
+                            <img src="./Assets/UserProfile.png" alt="Profile">
+                        </div>
+                        <div class="usrsp1left_02">
+                            <p>Cy Gullem</p>
+                            <span>a day ago &#183; <i class='fa-solid fa-user-group'></i></span>
+                        </div>
+                    </div>
+                    <div class="usrsp1right">
+                        <div class="usrsp1right_icon">
+                            <i class="fa-solid fa-ellipsis"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="usrsP_caption">
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Aperiam, impedit tempora provident quod sint exercitationem non omnis magnam nesciunt tenetur!
+                    </p>
+                </div>
+
+                <div class="usrsP_imagePosted">
+                    <img src="./Assets/lanscapeCanada.png" alt="Posted Image">
+                </div>
+
+                <div class="usrsP_activities">
+                    <div class="usrsP_ like">
+                        <i class='bx bx-like'></i>
+                        <p>Like</p>
+                    </div>
+                    <div class="usrsP_ comment">
+                        <i class="fa-regular fa-comment"></i>
+                        <p>Comment</p>
+                    </div>
+                    <div class="usrsP_ share">
+                        <i class='bx bx-share'></i>
+                        <p>Share</p>
+                    </div>
+                </div>
+
+                <div class="usrsP_comment">
+                    <div class="usrspcomL">
+                        <img src="<?php echo $_SESSION['profile_picture']; ?>" alt="Profile Image">
+                    </div>
+                    <div class="usrspcomR">
+                        <form action="">
+                            <input type="text" placeholder="Comment as <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname'];?>">
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
         <div class="content-Right">
