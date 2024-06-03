cd87b2c36f329c2a682c327e255a383381f38ebe
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-06-03 11:19:36 +0800

    New tables in the database: Like and Notifications

--- DATABASE/gullemfacebook.sql
+++ DATABASE/gullemfacebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 05:59 PM
+-- Generation Time: Jun 03, 2024 at 05:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,39 @@ CREATE TABLE `comment_table` (
 INSERT INTO `comment_table` (`id`, `user_id`, `post_id`, `comment`, `comment_time`) VALUES
 (3, 9, 6, 'I love it! and you ay natype hehe', '2024-05-10 14:58:45'),
 (8, 4, 6, 'nice kaayo 😍', '2024-05-10 09:29:59'),
-(9, 4, 17, 'commenting editing', '2024-05-10 15:53:56');
+(9, 4, 17, 'commenting editing', '2024-05-10 15:53:56'),
+(12, 23, 19, 'WOW that\'s dope', '2024-05-10 16:27:28'),
+(15, 24, 22, '101', '2024-05-10 10:42:18'),
+(17, 24, 19, 'amazing', '2024-05-10 10:44:49'),
+(18, 4, 19, 'thanks guys hehe, appreciated', '2024-05-11 01:32:22'),
+(19, 4, 23, 'you\'re dope', '2024-05-10 10:46:40'),
+(20, 4, 9, 'HAHAHHAHA', '2024-05-10 10:47:56'),
+(21, 10, 24, 'trhs sht ezz reeaalll', '2024-05-10 10:48:38'),
+(22, 10, 19, 'nice', '2024-05-10 10:48:48'),
+(24, 25, 25, 'shesh', '2024-05-10 19:43:03'),
+(25, 25, 19, 'shesh', '2024-05-10 19:43:46'),
+(26, 25, 19, 'hm', '2024-05-10 19:46:00'),
+(27, 4, 19, 'das dope', '2024-05-11 01:59:30'),
+(30, 26, 19, 'edit', '2024-05-10 20:04:50'),
+(31, 4, 9, 'hahahahahah', '2024-05-10 20:06:54'),
+(32, 4, 8, 'sheesh', '2024-05-10 20:07:07'),
+(33, 4, 28, 'paita naman ani uy. ako naman hinuon alanganin.', '2024-05-12 01:59:09'),
+(34, 9, 29, 'bagan hup 👊👊👊', '2024-05-16 20:27:56'),
+(36, 4, 6, 'hahhahaa yiieee 🤣🤣🤣🤣🤣', '2024-05-29 02:44:47'),
+(37, 4, 31, 'Created by Dhaniel', '2024-06-02 20:27:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `like_table`
+--
+
+CREATE TABLE `like_table` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -80,7 +112,25 @@ INSERT INTO `login_table` (`id`, `firstname`, `lastname`, `email`, `profile_pict
 (18, 'Jessa Mae', 'Redondo', 'jessa@gmail.com', 'ProfilePictures/6633b198e90fc_Jessa.png', '$2y$10$sS.BMALkRI6wZ.olaSd5pOC9NH0j4MYm/X4YVzQ5Nay5V6XvHb0xG', NULL, NULL, '2d08b6ce39597faea298e0de8a2abd28'),
 (19, 'Cathlenwin', 'Lawas', 'cathlennwin@gmail.com', 'ProfilePictures/6633b1ddbc4ed_Cathlenwin.png', '$2y$10$kk68o/N5LDr6D.oMDW9Bh.6cVz.0SuPbRq0it5gQV00RaNObyurqe', NULL, NULL, '3a469834d2efcd12fd6385a357c3e5a7'),
 (20, 'cyril', 'cyril', 'cyril@gmail.com', './Assets/default-profilepicture.png', '$2y$10$ZU7xu1BF00pdNvkXo3C.lucnCHbQwYeH7Zf2wgyjp3K7NgNi9Pleu', NULL, NULL, '9d9bae3b29422e47da16188306e4853d'),
-(21, 'Kriszia', 'Gullem', 'kriszia@gmail.com', './Assets/default-profilepicture.png', '$2y$10$3GUFVVerNusgyF7BJJKRvuP8sq58gU2sPrjFi9PD8jNhyderthXPe', NULL, NULL, '96e4f05abf7713847dce9dded5c7278a');
+(21, 'Kriszia', 'Gullem', 'kriszia@gmail.com', './Assets/default-profilepicture.png', '$2y$10$3GUFVVerNusgyF7BJJKRvuP8sq58gU2sPrjFi9PD8jNhyderthXPe', NULL, NULL, '96e4f05abf7713847dce9dded5c7278a'),
+(23, 'John', 'Doe', 'johndoe@gmail.com', 'ProfilePictures/663e4a96d53b2_Facebook-Logo.png', '$2y$10$QGBTPL93xzMzjN.I8b6kyeaoS1/UcgjDKJ1sy2kO3KzC7MOxugszu', NULL, NULL, '3d1239587e30e0a83b1f20f02a15fda5'),
+(24, 'John', 'Doe', 'johndoe12@gmail.com', 'ProfilePictures/663e4e2bba2e5_John_Doe.png', '$2y$10$6jUL2HTySp3NzpZsq9urBeP9AXm68iQEsD3boQEsrnilgBLsy8Yy6', NULL, NULL, '3ac5616a70aef329ed972de348e7b629'),
+(25, 'pagatpat', 'catwin', 'pagatpatcatwin@gmail.com', 'ProfilePictures/663eccf96dc20_Cathlenwin.png', '$2y$10$SYa9CrPDh9VqrCESXP4QqOWIjze/QGyj4q4qpjnaPXrVk1/23CP66', NULL, NULL, 'd81a2e92a3c4f60572f49d59627f9087'),
+(26, 'Zane', 'Daniel', 'zane@gmail.com', 'ProfilePictures/663ed1f622734_Zane_Daniel.png', '$2y$10$RJICgvVLgah8SARUVK8VIuvuJjqsYn5kn3bzSdbZgYrDECGMcGMEW', NULL, NULL, '9d8dbba4951ea5069d018b5f3c4b3be3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications_table`
+--
+
+CREATE TABLE `notifications_table` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,7 +154,11 @@ INSERT INTO `post_table` (`id`, `user_id`, `caption`, `imagePost`, `created_at`)
 (6, 9, 'I love Canada hehe mwuah', 'Post_Images/lanscapeCanada.png', '2024-05-05 15:07:33'),
 (8, 10, 'I am the greatest of all. HAHA', 'Post_Images/66383c1a3b60a_Dhaniel.png', '2024-05-06 02:10:34'),
 (9, 11, 'Dhaniel the great', NULL, '2024-05-06 02:12:12'),
-(19, 4, 'Birdland.', 'Post_Images/663e44145897a_Birdland.png', '2024-05-10 15:58:12');
+(20, 23, 'JOHN DOPE YEAAAAH', 'Post_Images/Switzerland.png', '2024-05-10 16:26:12'),
+(21, 23, 'HAHAHHA', NULL, '2024-05-10 16:26:51'),
+(23, 24, 'Dope', 'Post_Images/663e4ea52e694_Switzerland.png', '2024-05-10 16:43:17'),
+(26, 26, 'Zane', 'Post_Images/Zane_Daniel.png', '2024-05-11 02:03:43'),
+(31, 4, 'Birdland', 'Post_Images/665d2750928b6_Birdland.png', '2024-06-03 02:15:44');
 
 -- --------------------------------------------------------
 
@@ -154,7 +208,11 @@ INSERT INTO `user_following` (`id`, `follower_id`, `followed_id`, `created_at`) 
 (99, 19, 10, '2024-05-06 02:16:25'),
 (100, 19, 11, '2024-05-06 02:16:32'),
 (111, 12, 11, '2024-05-08 14:38:39'),
-(127, 4, 9, '2024-05-10 15:58:19');
+(129, 23, 4, '2024-05-10 16:27:46'),
+(131, 24, 4, '2024-05-10 16:43:54'),
+(137, 25, 4, '2024-05-11 01:43:29'),
+(139, 26, 4, '2024-05-11 02:05:00'),
+(145, 4, 9, '2024-05-29 02:24:59');
 
 --
 -- Indexes for dumped tables
@@ -167,9 +225,21 @@ ALTER TABLE `comment_table`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `like_table`
+--
+ALTER TABLE `like_table`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `login_table`
 --
 ALTER TABLE `login_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications_table`
+--
+ALTER TABLE `notifications_table`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -194,25 +264,37 @@ ALTER TABLE `user_following`
 -- AUTO_INCREMENT for table `comment_table`
 --
 ALTER TABLE `comment_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `like_table`
+--
+ALTER TABLE `like_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login_table`
 --
 ALTER TABLE `login_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `notifications_table`
+--
+ALTER TABLE `notifications_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `post_table`
 --
 ALTER TABLE `post_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `user_following`
 --
 ALTER TABLE `user_following`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
 
 --
 -- Constraints for dumped tables
