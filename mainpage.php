491aeed5e199c213474c4f9e669c32a0ca8de581
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-06-18 23:41:51 +0800

    Grid-images: not done yet

--- mainpage.php
+++ mainpage.php
@@ -44,6 +44,7 @@ $loggedInUserId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
     <link rel="icon" type="png" href="https://img.icons8.com/fluency/48/facebook-new.png">
     <title>Welcome to Facenote</title>
     <link rel="stylesheet" href="./CSS/mainpage.css">
+    <!-- <link rel="stylesheet" href="./CSS/grid-images.css"> -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- <script src="https://cdn.tailwindcss.com"></script>
